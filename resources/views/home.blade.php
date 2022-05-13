728de42c6d5ac231bba846437f45237750de6756
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2022-05-13 04:54:26 -0500

    Repo conectado con Github

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Prueba técnica</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Bienvenidx
-                </div>
-            </div>
+           
         </div>
     </div>
 </div>
