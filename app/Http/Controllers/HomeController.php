60c8b0855526412a1a52ceb381f6ea55ed05545c
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2022-05-16 10:25:44 -0500

    Home controller: listo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,8 +22,46 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function Index()
     {
-        return view('home');
+        $usuarios = User::all();
+        return view('home',compact('usuarios'));
+    }
+
+    public function Edit($id){
+        $user = User::all()->where('id',$id)->first();
+        return view('edit',compact('user'));
+    }
+
+    public function Show($id){
+        $user = User::all()->where('id',$id)->first();
+        return view('show',compact('user'));
+    }
+
+    public function Update(Request $request,$id){
+        $user = User::all()->where('id',$id)->first();
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->rfc = $request->rfc;
+        $user->notes = $request->notes;
+        $user->save();
+        return redirect()->route('index');
+    }
+
+    public function Delete($id){
+        $user = User::all()->where('id',$id)->first();
+        $user->delete();
+        return redirect()->route('index');
+    }
+
+    public function Create(Request $request){
+        $user = User::new();
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        $user->rfc = $request->rfc;
+        $user->password = $request->password;
+        $user->notes = $request->notes;
+        $user->save();
+        return redirect()->route('home');
     }
 }
